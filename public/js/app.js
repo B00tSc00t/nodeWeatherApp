d558be7e9c6949999a9a89eeb6e4ca2c14c2482f
B00tSc00t <saborick@gmail.com>
2021-09-22 16:13:18 -0600

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -13,7 +13,7 @@ weatherForm.addEventListener("submit", (e) => {
   messageTwo.textContent = '';
   // messageThree.textContent = 'Test';
   // fetch data from the api 'then' run a function
-  fetch("http://localhost:3000/weather?address=" + location).then((res) => {
+  fetch("/weather?address=" + location).then((res) => {
     res.json().then((data) => {
       if (data.err) {
         messageOne.textContent = data.err;
